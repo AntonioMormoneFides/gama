5135c6ce37bc94717737818dea01b953a17acd81
AntonioMormoneFides <antonio.mormone@fides.it>
2022-03-23 17:26:38 +0100

    Curriculum class and repository

---
+++ Backend/gama/src/main/java/com/project/gama/model/CurriculumModel.java
@@ -0,0 +1,64 @@
+package com.project.gama.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class CurriculumModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+
+	
+	@Column(length = 65535, columnDefinition="Text")
+	private String esperienzeStudio;
+	@Column(length = 65535, columnDefinition="Text")
+	private String esperienzeLavoro;
+	@Column(length = 65535, columnDefinition="Text")
+	private String autodescrizione;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEsperienzeStudio() {
+		return esperienzeStudio;
+	}
+	public void setEsperienzeStudio(String esperienzeStudio) {
+		this.esperienzeStudio = esperienzeStudio;
+	}
+	public String getEsperienzeLavoro() {
+		return esperienzeLavoro;
+	}
+	public void setEsperienzeLavoro(String esperienzeLavoro) {
+		this.esperienzeLavoro = esperienzeLavoro;
+	}
+	public String getAutodescrizione() {
+		return autodescrizione;
+	}
+	public void setAutodescrizione(String autodescrizione) {
+		this.autodescrizione = autodescrizione;
+	}
+	public CurriculumModel(Integer id,  String esperienzeStudio, String esperienzeLavoro,
+			String autodescrizione) {
+		super();
+		this.id = id;
+		
+		this.esperienzeStudio = esperienzeStudio;
+		this.esperienzeLavoro = esperienzeLavoro;
+		this.autodescrizione = autodescrizione;
+	}
+	
+	
+	public CurriculumModel() {
+		
+	};
+	
+}
---
+++ Backend/gama/src/main/java/com/project/gama/repository/CurriculumRepository.java
@@ -0,0 +1,9 @@
+package com.project.gama.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.gama.model.CurriculumModel;
+
+public interface CurriculumRepository extends JpaRepository<CurriculumModel, Integer>{
+
+}
